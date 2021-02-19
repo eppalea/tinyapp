177a380ee369691cfd454be98986865c73719bc9
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-19 07:41:57 +0000

    moved the urlsForUser function from express_server.js to helpers.js

--- helpers.js
+++ helpers.js
@@ -8,4 +8,20 @@ const emailChecker = function(email, database) {
   }
 };
 
-module.exports = { emailChecker };
+
+const urlsForUser = function(urlDatabase, id) {
+  let userSpecificURLDatabase = {};
+  console.log("the urlDatabase is: ", urlDatabase);
+  for (const shortURL in urlDatabase) {
+    // console.log("the shortURL is: ", shortURL);
+    // console.log("the url db userid is: ", urlDatabase[shortURL].userID)
+    // console.log("this is id: ", id);
+    if (urlDatabase[shortURL].userID === id) {
+      userSpecificURLDatabase[shortURL] = urlDatabase[shortURL];
+      // console.log("the user specific db key is:" ,userSpecificURLDatabase[shortURL]);
+    }
+  }
+  return userSpecificURLDatabase;
+};
+
+module.exports = { emailChecker, urlsForUser };
