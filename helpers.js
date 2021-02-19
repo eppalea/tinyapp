894166396d44e1fe9c6c0d0055b6dbbd2da23b19
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-19 23:09:08 +0000

    eslinted and removed comments

--- helpers.js
+++ helpers.js
@@ -1,10 +1,10 @@
 const emailChecker = function(email, database) {
   for (const user in database) {
     if (email === database[user].email) {
-      console.log("the return is:", database[user])
+      console.log("the return is:", database[user]);
       return database[user];
-    } 
-  return false;
+    }
+    return false;
   }
 };
 
@@ -13,12 +13,8 @@ const urlsForUser = function(urlDatabase, id) {
   let userSpecificURLDatabase = {};
   console.log("the urlDatabase is: ", urlDatabase);
   for (const shortURL in urlDatabase) {
-    // console.log("the shortURL is: ", shortURL);
-    // console.log("the url db userid is: ", urlDatabase[shortURL].userID)
-    // console.log("this is id: ", id);
     if (urlDatabase[shortURL].userID === id) {
       userSpecificURLDatabase[shortURL] = urlDatabase[shortURL];
-      // console.log("the user specific db key is:" ,userSpecificURLDatabase[shortURL]);
     }
   }
   return userSpecificURLDatabase;
