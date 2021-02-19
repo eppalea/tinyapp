183c8ac60ea137d7231a8a60e11669d0bc5e1456
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-19 02:23:55 +0000

    created a new file to hold my emailChecker function. export it to the express_server.js file

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const emailChecker = function(email, database) {
+  for (const user in database) {
+    if (email === database[user].email) {
+      return database[user];
+    } 
+  return false;
+  }
+};
+
+module.exports = emailChecker;
