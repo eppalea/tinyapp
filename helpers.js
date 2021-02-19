a39fc6c9d548f6737de75ad1df229d78775f1bd1
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-19 05:24:52 +0000

    changed the module.exports into an object

--- helpers.js
+++ helpers.js
@@ -1,10 +1,11 @@
 const emailChecker = function(email, database) {
   for (const user in database) {
     if (email === database[user].email) {
+      console.log("the return is:", database[user])
       return database[user];
     } 
   return false;
   }
 };
 
-module.exports = emailChecker();
+module.exports = { emailChecker }
