2efb63111502c4becf6130b7c0901e83b583ab02
Eppa Lea Turniawan <eppalea@gmail.com>
2021-02-19 21:35:29 +0000

    eslinted

--- express_server.js
+++ express_server.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const cookieSession = require('cookie-session');
+const bodyParser = require("body-parser");
+const bcrypt = require('bcryptjs');
 const { emailChecker, urlsForUser } = require('./helpers');
 
-const cookieSession = require('cookie-session');
+
+app.set("view engine", "ejs");
 app.use(cookieSession({
   name: 'session',
   keys: ['any', 'values'],
 }));
-
-app.set("view engine", "ejs");
-
-const bodyParser = require("body-parser");
-const bcrypt = require('bcryptjs');
 app.use(bodyParser.urlencoded({extended: true}));
+
 
 const generateRandomString = function(length) {
   return Math.random().toString(36).substr(2, length);
@@ -27,9 +26,11 @@ const urlDatabase = {
   "9sm5xK": { longURL: "http://www.google.com", userID: "userRandomID2" }
 };
 
+
 app.get("/", (req, res) => {
   res.redirect("/login");
 });
+
 
 app.get("/register", (req, res) => {
   const user = users[req.session["user_id"]];
@@ -38,6 +39,7 @@ app.get("/register", (req, res) => {
   };
   res.render("register", templateVars);
 });
+
 
 app.post("/register", (req, res) => {
   if (!req.body.email) {
@@ -60,12 +62,14 @@ app.post("/register", (req, res) => {
   res.redirect('/urls');
 });
 
+
 app.get('/login', (req, res) => {
   const templateVars = {
     user: null,
   };
   res.render("login", templateVars);
 });
+
 
 app.post('/login', (req, res) => {
   if (!emailChecker(req.body.email, users)) {
@@ -81,10 +85,12 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+
 app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect("/urls");
 });
+
 
 app.get("/urls", (req, res) => {
   const user = users[req.session["user_id"]];
@@ -100,14 +106,16 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", userTemplateVars);
 });
 
+
 app.get("/urls/new", (req, res) => {
   const user = users[req.session["user_id"]];
-  const templateVars = { user: user,};
+  const templateVars = { user: user };
   if (!user) {
     res.redirect('/login');
   }
   res.render("urls_new", templateVars);
 });
+
 
 app.get("/urls/:shortURL", (req, res) => {
   const user = users[req.session["user_id"]];
@@ -116,20 +124,23 @@ app.get("/urls/:shortURL", (req, res) => {
     return;
   }
   if (user.id !== urlDatabase[req.params.shortURL].userID) {
-    res.send("Access denied. You don't own this shortURL.")
+    res.send("Access denied. You don't own this shortURL.");
+    return;
   }
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL,
     user: user
   };
-  res.render("urls_show", templateVars);  
+  res.render("urls_show", templateVars);
 });
+
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL].longURL;
   res.redirect(longURL);
 });
+
 
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString(6);
@@ -146,12 +157,13 @@ app.post("/urls/:shortURL", (req, res) => {
     return;
   }
   if (user.id !== urlDatabase[req.params.shortURL].userID) {
-    res.send("Access denied. You don't own this shortURL.")
+    res.send("Access denied. You don't own this shortURL.");
+    return;
   }
   const shortURL = req.params.shortURL;
   const updatedlongURL = req.body.longURL;
   urlDatabase[shortURL].longURL = updatedlongURL;
-  res.redirect('/urls');  
+  res.redirect('/urls');
 });
 
 
@@ -163,7 +175,8 @@ app.post('/urls/:shortURL/delete', (req, res) => {
     return;
   }
   if (user.id !== urlDatabase[req.params.shortURL].userID) {
-    res.send("Access denied. You don't own this shortURL.")
+    res.send("Access denied. You don't own this shortURL.");
+    return;
   }
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
